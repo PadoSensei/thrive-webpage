fcfbf4a755e090165cf79a000b0a04fa64f4f1b6
PadoSensei <padraigo.donoghue@gmail.com>
2022-11-16 20:56:33 +0000

    New Layout

--- src/components/StoryCarousel.tsx
+++ src/components/StoryCarousel.tsx
@@ -1,15 +1,21 @@
 import { Container, Group, Button, Text} from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
 import { AppCard } from './AppCard';
 
 export function StoryCarousel() {
     return (
         <Group position="apart">
-          <Container size="xs" px="xs">
-            <AppCard></AppCard>
-          </Container>
-          <Container size="xs" px="xs">
-            <Button>Donate Now!</Button>
-          </Container>
+          <Carousel sx={{ maxWidth: 320 }} mx="auto" withIndicators height={200}>
+            <Carousel.Slide>
+              <AppCard />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <AppCard />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <AppCard />
+            </Carousel.Slide>
+          </Carousel>
         </Group>
 
     )
---
+++ src/site-copy-pt.js
@@ -0,0 +1,26 @@
+const copy_pt_first = ['A Associação Brasileira de Combate ao Câncer Pedro Correa (Abracce) existe ajudar pessoas e famílias que vivem em condições de vulnerabilidade e lutam contra o câncer em Belo Horizonte e região metropolitana - uma vez que, além da doença, algumas pessoas ainda enfrentam dificuldades como o acesso a um tratamento de qualidade onde moram, não possuem condições financeiras e precisam ir até outras cidades, muitas vezes desamparados, para realizar um tratamento.']
+
+const copy_pt_second = ['Conheça a Abracce', 
+'Desde 2010, a Abracce Pedro Corrêa apoia pessoas que lutam contra o câncer através do acolhimento de pacientes e seus acompanhantes que viajam para Belo Horizonte, MG, em busca de tratamento.'
+, 'Devido às limitações impostas pela doença, à falta de recursos financeiros e também pela distância de suas cidades de origem, a ONG oferece, de forma gratuita, acomodação, alimentação, acompanhamento por equipe multidisciplinar e deslocamento ao hospital.',
+ 'Localizada no bairro Carlos Prates, a instituição conta com uma casa de estrutura adequada, profissionais treinados para recebê-los em um ambiente seguro, alegre e acolhedor. Também recebe a colaboração de voluntários de terapia holística, psicóloga, alunos de escolas públicas e cursos profissionalizantes, dentre outros. 
+, 'A Abracce, além disso, oferece atendimentos externos a pacientes de vulnerabilidade social que moram em Belo Horizonte e Região Metropolitana através do acolhimento por um Assistente Social, que realiza um estudo socioeconômico para entender as necessidades de cada indivíduo e melhorar a qualidade de vida.'
+]
+
+const copy_pt_third = [
+    'Como tudo começou',
+    'A Abracce surgiu do desejo de viver do Sr. Pedro Corrêa, que, em 2007, saiu de Rondônia junto com a esposa e dois filhos e foi para Belo Horizonte em busca de tratamento de câncer.',
+    'Durante os 92 dias em que ficou internado, ele e sua família puderam acompanhar de perto a dificuldade de centenas de pessoas que, assim como eles, saiam de cidades distantes para serem tratadas.  Mais do que sentir na pele, viram o quanto é difícil enfrentar a doença, passar pelo tratamento durante o dia e ainda terem que viajar longas distâncias para retornarem à suas casas ou passarem a noite em acomodações improvisadas pelos corredores dos hospitais.',
+    'Foi assim que Pedro Corrêa compartilhou com sua família o desejo de abrir um abrigo onde acomodaria estas pessoas, proporcionando acomodação, alimentação, conforto e um ambiente humanizado durante todo o tratamento.',
+    'Infelizmente, seis meses depois de iniciar o seu tratamento, Pedro faleceu, mas, em 2010, sua filha Mariza fez com que o desejo do pai se tornasse realidade.'
+]
+
+const copy_pt_fourth = [
+    'DOE!',
+    'O dinheiro será usado para manter o funcionamento da casa, que envolve o pagamento de remédios, alimentação e transporte dos pacientes, além das despesas com os colaboradores. As doações podem ser feitas através deste link: https://www.padrim.com.br/abraccepedrocorrea',
+]
+
+const copy_pt_fifth = [
+    'Mariza Corrêa - 31 98645-1733',
+    'Facebook | Instagram | ADD DOMAIN'
+]
